c90015031e1595b155309e5704f15c7c34c59eed
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-08-12 11:29:02 +0100

    tail recursive solution to problem 4

--- lib/matt_things.ml
+++ lib/matt_things.ml
@@ -38,12 +38,24 @@ let%test "should return nth element of string list" = nth [ "a"; "b"; "c"; "d" ]
 let%test "should return 0th element" = nth [ 'a'; 'b' ] 0 = 'a'
 
 (*problem 4*)
-let rec length l = 
+let rec length l =
   match l with
-    | [] -> 0
-    | _ :: tail -> 1 + length tail
+  | [] -> 0
+  | _ :: tail -> 1 + length tail
 ;;
 
-let%test "should return length of list" = length [0; 1; 2] = 3
+let%test "should return length of list" = length [ 0; 1; 2 ] = 3
 let%test "should return length of empty list" = length [] = 0
 
+(*problem 4 tail recursive*)
+let length_tail_rec l =
+  let rec go ls acc =
+    match ls with
+    | [] -> acc
+    | _ :: tail -> go tail (acc + 1)
+  in
+  go l 0
+;;
+
+let%test "should return length of list" = length_tail_rec [ 0; 1; 2 ] = 3
+let%test "should return length of empty list" = length_tail_rec [] = 0
