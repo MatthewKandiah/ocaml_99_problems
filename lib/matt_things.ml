be93f86730ae4ecbe2694bec3eba7ff76c339c89
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-08-12 09:23:34 +0100

    problem2

--- lib/matt_things.ml
+++ lib/matt_things.ml
@@ -7,3 +7,14 @@ let rec last l = match l with
 
 let%test "should return last element" = last [ "a"; "b"; "c"; "d" ] = Some "d"
 let%test "should handle empty list" = last [] = None
+
+let rec last_two l = match l with
+  | [] | [ _ ] -> None
+  | [ x; y ] -> Some(x, y)
+  | _ :: tail -> last_two tail
+  ;;
+
+let%test "should return last two elements" = last_two ["a"; "b"; "c"; "d"] = Some ("c", "d")
+let%test "should handle single element list" = last_two ["a"] = None
+let%test "should handle empty list" = last_two [] = None
+
