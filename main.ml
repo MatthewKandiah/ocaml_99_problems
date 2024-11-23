99dedfaedc22b0018a370e782d7c0099a59ff0d5
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-11-23 08:33:23 +0000

    p2 done

--- main.ml
+++ main.ml
@@ -11,3 +11,18 @@ let rec last lst =
 assert (last [1; 2; 3] = Some 3);;
 assert (last ['a'] = Some 'a');;
 assert (last [] == None);;
+
+(*
+  Problem 2 - Last two elements of a list
+*)
+
+let rec last_two lst =
+  match lst with
+  | [] -> None
+  | [x] -> None
+  | [x; y] -> Some (x , y)
+  | _ :: rest -> last_two rest;;
+
+assert (last_two ["a"; "b"; "c"; "d"] = Some ("c", "d"));;
+assert (last_two [1] = None);;
+assert (last_two [] = None);;
