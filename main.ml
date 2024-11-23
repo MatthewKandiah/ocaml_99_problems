0a3f9ce963ed94425976ad8430218239bef24317
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-11-23 08:43:29 +0000

    p3 done

--- main.ml
+++ main.ml
@@ -26,3 +26,22 @@ let rec last_two lst =
 assert (last_two ["a"; "b"; "c"; "d"] = Some ("c", "d"));;
 assert (last_two [1] = None);;
 assert (last_two [] = None);;
+
+(*
+  Problem 3 - N'th element of a list
+*)
+
+let rec nth lst n =
+  match (lst, n) with
+  | [], _ -> None
+  | x :: _, 0 -> Some x
+  | _ :: rest, m -> nth rest (m-1);;
+
+assert (nth ["a"; "b"; "c"; "d"; "e"] 0 = Some "a");;
+assert (nth ["a"; "b"; "c"; "d"; "e"] 1 = Some "b");;
+assert (nth ["a"; "b"; "c"; "d"; "e"] 2 = Some "c");;
+assert (nth ["a"; "b"; "c"; "d"; "e"] 3 = Some "d");;
+assert (nth ["a"; "b"; "c"; "d"; "e"] 4 = Some "e");;
+assert (nth ["a"; "b"; "c"; "d"; "e"] 5 = None);;
+assert (nth [] 0 = None);;
+assert (nth [] 5 = None);;
